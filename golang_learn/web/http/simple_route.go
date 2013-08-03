7e16eb2f06b84256fd69c8a4b5ae770bf4dff37b
atupal <me@atupal.org>
2013-08-03 15:56:26 +0800

    simple_route

---
+++ golang_learn/web/http/simple_route.go
@@ -0,0 +1,29 @@
+
+package main
+
+import (
+  "fmt"
+  "net/http"
+)
+
+type MyMux struct {
+}
+
+func (p *MyMux) ServeHTTP(w http.ResponseWriter, r *http.Request) {
+  if r.URL.Path == "/" {
+    sayhelloName(w, r)
+    return
+  }
+  http.NotFound(w, r)
+  return
+}
+
+func sayhelloName(w http.ResponseWriter, r *http.Request) {
+  fmt.Fprintf(w, "Hello myroute!")
+}
+
+func main() { 
+  mux := &MyMux{}
+  http.ListenAndServe(":9090", mux)
+
+}
