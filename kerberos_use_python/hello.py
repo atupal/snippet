6ad8d76dc98f17dddfefbdd9a5e0b14e816249a3
atupal <me@atupal.org>
2013-08-07 16:59:30 +0800

    negotiate

--- kerberos_use_python/hello.py
+++ kerberos_use_python/hello.py
@@ -2,6 +2,7 @@
 # -*- coding: utf-8 -*-
 
 import requests
+import kerberos
 from pprint import pprint as printf
 
 URL = "http://10.2.201.65:32201/webhdfs/v1/?op=liststatus"
@@ -12,6 +13,11 @@ def un_auth():
   print resp.headers
   print resp.content
 
+def negotiate_auth():
+  _, krb_context = kerberos.authGSSClientInit("h_yukangle@xxx.xxx")
+  kerberos.authGSSClientStep(krb_context, "")
+
  
 if __name__ == "__main__":
-  un_auth()
+  #un_auth()
+  negotiate_auth()
