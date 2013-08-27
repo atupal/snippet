a9cda002efbba770c03d94aa7f867befef41239f
atupal <me@atupal.org>
2013-08-27 10:27:39 +0800

    updatwe

--- hbase_thriftapi/hello.py
+++ hbase_thriftapi/hello.py
@@ -1,13 +1,15 @@
 # -*- coding: utf-8 -*-
 
+import sys, os
+sys.path.append(os.path.join(os.path.realpath('.'), 'gen-py'))
 from thrift.transport.TSocket import TSocket
 from thrift.transport.TTransport import TBufferedTransport
 from thrift.protocol import TBinaryProtocol
-from gen_py.hbase import Hbase
+from hbase import Hbase
 
 transport = TBufferedTransport(TSocket('10.237.14.236', 9090))
 transport.open()
 protocol = TBinaryProtocol.TBinaryProtocol(transport)
 
 client = Hbase.Client(protocol)
-print help(client)
+print client.getTableNames()
