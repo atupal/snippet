55b55308d5b65bd570639492d5e748954e1b23e3
atupal <me@atupal.org>
2013-08-28 16:48:20 +0800

    update

--- hbase_thriftapi/hello.py
+++ hbase_thriftapi/hello.py
@@ -6,10 +6,30 @@ from thrift.transport.TSocket import TSocket
 from thrift.transport.TTransport import TBufferedTransport
 from thrift.protocol import TBinaryProtocol
 from hbase import Hbase
+from hbase.ttypes import ColumnDescriptor
 
-transport = TBufferedTransport(TSocket('10.237.14.236', 9090))
-transport.open()
-protocol = TBinaryProtocol.TBinaryProtocol(transport)
 
-client = Hbase.Client(protocol)
-print client.getTableNames()
+
+
+class HbaseClient(object):
+  def __init__(self, host='10.237.14.236', port=9090):
+    transport = TBufferedTransport(TSocket(host, port))
+    transport.open()
+    protocol = TBinaryProtocol.TBinaryProtocol(transport)
+    self.client = Hbase.Client(protocol)
+
+  def getTableNames(self):
+    return self.client.getTableNames()
+
+  def createTable(self, tableName, columnFamilies):
+    columnFamilies = [ ColumnDescriptor(_) for _ in columnFamilies ]
+    return self.client.createTable(tableName, columnFamilies)
+
+  def exec_(self, name, *args, **kwargs):
+    return eval('self.client.%(name)s(*%(args)s, **%(kwargs)s)' % ({'name': name, 'args': args, 'kwargs': kwargs}) )
+
+if __name__ == "__main__":
+  hclient = HbaseClient()
+  print hclient.createTable('test_atupal_2', ['atupal'])
+  #print hclient.exec_('getTableRegions', 'foo')
+
