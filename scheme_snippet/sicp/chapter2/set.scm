5fd3bacd56ff96459f53f7a63161743cd5f0bff5
atupal <atupalykl@gmail.com>
2015-04-30 13:45:29 +0000

    fix bug

--- scheme_snippet/sicp/chapter2/set.scm
+++ scheme_snippet/sicp/chapter2/set.scm
@@ -37,15 +37,43 @@
   (copy-to-list tree '()))
 
 
+(define (list->tree elements)
+  (car (partial-tree elements (length elements))))
+
+(define (partial-tree elts n)
+  (if (= n 0)
+    (cons '() elts)
+    (let ((left-size (quotient (- n 1) 2)))
+      (let ((left-result 
+              (partial-tree elts left-size)))
+        (let ((left-tree (car left-result))
+              (non-left-elts (cdr left-result))
+              (right-size (- n (+ left-size 1))))
+          (let ((this-entry (car non-left-elts))
+                (right-result
+                  (partial-tree
+                    (cdr non-left-elts)
+                    right-size)))
+            (let ((right-tree (car right-result))
+                  (remaining-elts
+                    (cdr right-result)))
+              (cons (make-tree this-entry
+                               left-tree
+                               right-tree)
+                    remaining-elts))))))))
+
 
 (define tree
   (adjoin-set 4
               (adjoin-set 5
                           (adjoin-set 2
-                                      (adjoin-set 0 
-                                                  (adjoin-set 1 
+                                      (adjoin-set 0
+                                                  (adjoin-set 1
                                                               (make-tree 3 '() '())))))))
 
 (display (tree->list-1 tree))  ; O(nlgn)
 (display "\n")
 (display (tree->list-2 tree))  ; O(n)
+(display "\n")
+(display (tree->list-1 (list->tree (list 1 2 3 4 5 6 7 8 9))) )
+
