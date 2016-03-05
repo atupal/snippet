2baf1bcfcae39e0221754f9d5bcce066439b7d15
atupal <atupalykl@gmail.com>
2016-03-05 23:07:10 +0000

    update

--- scheme_snippet/sicp/chapter3/stream.scm
+++ scheme_snippet/sicp/chapter3/stream.scm
@@ -548,3 +548,26 @@
 
 ; end exercise 3.76
 
+
+; Streams and Delayed Evaluation
+
+(define (integral-delayed delayed-integrand initial-value dt)
+  (define int
+    (cons-stream
+      initial-value
+      (let ((integrand (force delayed-integrand)))
+        (add-streams (scale-stream integrand dt) int))))
+  int)
+
+(define (solve f y0 dt)
+  (define y (integral-delayed (delay dy) y0 dt))
+  (define dy (stream-map f y))
+  y)
+
+;(display-line (stream-ref (solve (lambda (y) y)
+                   ;1
+                   ;0.001)
+            ;1000))
+
+
+
