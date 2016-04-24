6a817aa0f9d251143fff00cfc65fdeadb8f6a87e
atupal <me@atupal.org>
2016-04-24 02:57:44 +0000

    exercise 4.21

--- scheme_snippet/sicp/chapter4/test_evaluator.scm
+++ scheme_snippet/sicp/chapter4/test_evaluator.scm
@@ -52,3 +52,10 @@
            (if (= n 1) 1 (* n (fact (- n 1)))))))
   (fact 10))
 ; end exercise 4.20
+
+; start exercise 4.21
+((lambda (n)
+   ((lambda (fact) (fact fact n))
+    (lambda (ft k) (if (= k 1) 1 (* k (ft ft (- k 1)))))))
+ 10)
+; end exercise 4.21
