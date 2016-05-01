9d64fcb9a27e8b91233b80f1207578e1e2c0910f
atupal <me@atupal.org>
2016-05-01 13:02:02 +0000

    Streams as Lazy Lists

--- scheme_snippet/sicp/chapter4/test_evaluator.scm
+++ scheme_snippet/sicp/chapter4/test_evaluator.scm
@@ -157,3 +157,48 @@ count
 ; 
 ; count ; 1
 ; end exercise 4.31
+
+; start 4.2.3 Streams as Lazy Lists
+(define (cons x y) (lambda (m) (m x y)))
+(define (car z) (z (lambda (p q) p)))
+(define (cdr z) (z (lambda (p q) q)))
+
+(define (list-ref items n)
+  (if (= n 0)
+    (car items)
+    (list-ref (cdr items) (- n 1))))
+
+(define (map proc items)
+  (if (null? items)
+    '()
+    (cons (proc (car items)) (map proc (cdr items)))))
+
+(define (scale-list items factor)
+  (map (lambda (x) (* x factor)) items))
+
+(define (add-lists list1 list2)
+  (cond ((null? list1) list2)
+        ((null? list2) list1)
+        (else (cons (+ (car list1) (car list2))
+                    (add-lists (cdr list1) (cdr list2))))))
+
+(define ones (cons 1 ones))
+
+(define integers (cons 1 (add-lists ones integers)))
+
+(list-ref integers 17)
+
+
+(define (integral integrand initial-value dt)
+  (define int
+    (cons initial-value
+          (add-lists (scale-list integrand dt) int)))
+  int)
+
+(define (solve f y0 dt)
+  (define y (integral dy y0 dt))
+  (define dy (map f y))
+  y)
+
+; these will cause stack overflow of guile vm, but mit-scheme works
+;(list-ref (solve (lambda (x) x) 1 0.001) 1000)
