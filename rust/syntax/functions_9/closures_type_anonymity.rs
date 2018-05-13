bb313cf874488ab7414b8a4ba523dd23c2e348e2
atupal <atupalykl@gmail.com>
2018-05-14 02:34:34 +0800

    Finish 9

---
+++ rust/syntax/functions_9/closures_type_anonymity.rs
@@ -0,0 +1,16 @@
+// `F` must implement `Fn` for a closure which takes no
+// inputs and returns nothing - exactly what is required
+// for `print`.
+fn apply<F>(f: F) where F: Fn() {
+    f();
+}
+
+fn main() {
+    let x = 7;
+
+    // Capture `x` ito an anonymous type and implement
+    // `Fn` for it. Store it in `print`.
+    let print = || println!("{}", x);
+
+    apply(print);
+}
